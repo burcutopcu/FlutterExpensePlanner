091d61584cf2016ff2152366b389063621eda184
burcutopcu <>
2020-04-02 21:13:22 +0300

    transaction list and transaction item model added

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-
-import 'transaction.dart';
+import 'package:flutterexpenseplanner/widgets/user_transaction.dart';
 
 void main() {
   runApp(MyApp());
@@ -20,59 +19,39 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  final List<Transaction> transactions = [
-    Transaction(
-        id: 't1', title: 'New Shoes', amount: 69.99, date: DateTime.now()),
-    Transaction(
-        id: 't2', title: 'Weekly Grocery', amount: 16.53, date: DateTime.now())
-  ];
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: Text('App'),
+          actions: <Widget>[
+            IconButton(icon: Icon(Icons.add), onPressed: () {
+
+            },)
+          ],
         ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+      body: SingleChildScrollView(
+        child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Container(
-              width: double.infinity,
-              child: Card(
-                color: Colors.pink,
-                child: Text('CHART!'),
-              ),
-            ),
-            Column(
-                children: transactions.map((tx) {
-              return Card(
-                  child: Row(
-                children: <Widget>[
-                  Container(
-                    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 14),
-                    decoration: BoxDecoration(
-                        border: Border.all(color: Colors.purple, width: 2)),
-                    padding: EdgeInsets.all(10),
-                    child: Text(
-                      '\$ ${tx.amount}',
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
-                          color: Colors.purple),
-                    ),
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(tx.title, style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),),
-                      Text(tx.date.toString(),style: TextStyle(color: Colors.grey),)
-                    ],
-                  )
-                ],
-              ));
-            }).toList())
+//            Container(
+//              width: double.infinity,
+//              child: Card(
+//                color: Colors.pink,
+//                child: Text('CHART!'),
+//              ),
+//            ),
+            UserTransaction()
           ],
-        ));
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+
+        },
+      ),
+    );
   }
 }
